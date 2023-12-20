325b699483b2acdc0dd96926935afae2fa46a2cd
sravan <gontlasravan@gmail.com>
2023-12-20 11:23:47 +0530

    Initial commit

---
+++ src/main/java/com/vk/postgres/hazelcast/debenzium/logicalreplication/LogicalreplicationApplication.java
@@ -0,0 +1,13 @@
+package com.vk.postgres.hazelcast.debenzium.logicalreplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogicalreplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogicalreplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vk/postgres/hazelcast/debenzium/logicalreplication/LogicalreplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vk.postgres.hazelcast.debenzium.logicalreplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogicalreplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
